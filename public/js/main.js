0a2e0f9f8d8a5b1701985bc37b417faab5a6091e
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-06-05 15:26:43 -0500

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var API_URL = 'http://api.wunderground.com/api/459997bb99f562c6/conditions/geolookup/forecast10day/q/';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9qcy9tYWluLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsSUFBSSxPQUFPLEdBQUcsd0ZBQXdGLENBQUMiLCJmaWxlIjoic3JjL2pzL21haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJ2YXIgQVBJX1VSTCA9ICdodHRwOi8vYXBpLnd1bmRlcmdyb3VuZC5jb20vYXBpLzQ1OTk5N2JiOTlmNTYyYzYvY29uZGl0aW9ucy9nZW9sb29rdXAvZm9yZWNhc3QxMGRheS9xLyc7XG4iXX0=
---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+var API_URL = 'http://api.wunderground.com/api/459997bb99f562c6/conditions/geolookup/forecast10day/q/';
